e3fa7bdf2b1506618904bf670b1a6955288ee956
Dan <Dbyrne@tactiq.co.uk>
2020-09-17 17:02:47 +0100

    allowing opperation if no wifi

--- lib/wifiOTA/wifiOTA.cpp
+++ lib/wifiOTA/wifiOTA.cpp
@@ -6,24 +6,43 @@
 
 #include "../../include/credentials.h"
 
+static bool connectedToWifi = false;
+static unsigned long lastAttempt = 0;
+
+#define RETRY_WIFI_TIME_MINS ((10 * 60) * 60)
+
 void wifiOTAInit(void)
 {
     WiFi.mode(WIFI_STA);
     WiFi.setHostname(HOSTNAME);
     WiFi.begin(WIFI_SSID, WIFI_PASS);
-    while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-        Serial.println("Connection Failed! Rebooting...");
-        delay(5000);
-        ESP.restart();
+    if (WiFi.waitForConnectResult() == WL_CONNECTED)
+    {
+        ArduinoOTA.setHostname(HOSTNAME);
+        ArduinoOTA.begin();
     }
-
-    ArduinoOTA.setHostname(HOSTNAME);
-
-    ArduinoOTA.begin();
 }
 
 
 void wifiOTACheck(void)
 {
-    ArduinoOTA.handle();
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        ArduinoOTA.handle();
+    }
+    else
+    {
+        unsigned long currentMillis = millis();
+        if (currentMillis - lastAttempt >= RETRY_WIFI_TIME_MINS)
+        {
+            WiFi.begin(WIFI_SSID, WIFI_PASS);
+            if (WiFi.waitForConnectResult() == WL_CONNECTED)
+            {
+                ArduinoOTA.setHostname(HOSTNAME);
+                ArduinoOTA.begin();
+                lastAttempt = currentMillis;
+                connectedToWifi = true;
+            }
+        }
+    }
 }
